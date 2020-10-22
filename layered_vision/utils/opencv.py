e27804bdc1ec44f84632da0c1af868ca3bbb14a3
Daniel Ecer <de-code@users.noreply.github.com>
2020-10-22 23:47:05 +0100

    added delay to keep video frame rate

--- layered_vision/utils/opencv.py
+++ layered_vision/utils/opencv.py
@@ -1,4 +1,5 @@
 import logging
+from time import time, sleep
 from contextlib import contextmanager
 from typing import ContextManager, Iterable
 
@@ -14,9 +15,12 @@ LOGGER = logging.getLogger(__name__)
 def iter_read_video_images(
     video_capture: cv2.VideoCapture,
     image_size: ImageSize = None,
-    interpolation: int = cv2.INTER_LINEAR
+    interpolation: int = cv2.INTER_LINEAR,
+    fps: float = None
 ) -> Iterable[np.ndarray]:
     is_first = True
+    desired_frame_time = 1 / fps if fps else 0
+    last_frame_time = None
     while True:
         grabbed, image_array = video_capture.read()
         if not grabbed:
@@ -34,7 +38,15 @@ def iter_read_video_images(
                 (image_size.width, image_size.height),
                 interpolation=interpolation
             )
-        yield bgr_to_rgb(image_array)
+        rgb_image_array = bgr_to_rgb(image_array)
+        current_time = time()
+        if last_frame_time:
+            desired_wait_time = desired_frame_time - (current_time - last_frame_time)
+            if desired_wait_time > 0:
+                LOGGER.debug('sleeping for desired fps: %s', desired_wait_time)
+                sleep(desired_wait_time)
+        last_frame_time = time()
+        yield rgb_image_array
         is_first = False
 
 
@@ -43,9 +55,19 @@ def get_video_image_source(
     path: str,
     image_size: ImageSize = None
 ) -> ContextManager[Iterable[np.ndarray]]:
+    LOGGER.info('loading video: %r', path)
     video_capture = cv2.VideoCapture(path)
+    actual_image_size = ImageSize(
+        width=video_capture.get(cv2.CAP_PROP_FRAME_WIDTH),
+        height=video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    )
+    actual_fps = video_capture.get(cv2.CAP_PROP_FPS)
+    LOGGER.info(
+        'video reported image size: %s (%s fps)',
+        actual_image_size, actual_fps
+    )
     try:
-        yield iter_read_video_images(video_capture, image_size=image_size)
+        yield iter_read_video_images(video_capture, image_size=image_size, fps=actual_fps)
     finally:
         LOGGER.debug('releasing video capture: %s', path)
         video_capture.release()
