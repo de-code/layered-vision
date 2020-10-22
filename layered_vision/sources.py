6f97775fa175184efaa76372f5f72fa66b65be3e
Daniel Ecer <de-code@users.noreply.github.com>
2020-10-23 01:11:26 +0200

    added video source support (#2)
    * raise an exception if image couldn't be loaded
    
    * strip query string from file path
    
    * added video source support
    
    * added delay to keep video frame rate
    
    * moved repeat closer to source
    
    * attempting to set video capture size
    
    * added fps config option

--- layered_vision/sources.py
+++ layered_vision/sources.py
@@ -1,11 +1,14 @@
 import logging
+import os
 from contextlib import contextmanager
+from itertools import cycle
 from typing import ContextManager, Iterable
 
 import cv2
 
 from .utils.image import resize_image_to, ImageSize, ImageArray, bgr_to_rgb
-from .utils.io import get_file
+from .utils.io import get_file, strip_url_suffix
+from .utils.opencv import get_video_image_source
 
 
 LOGGER = logging.getLogger(__name__)
@@ -18,15 +21,30 @@ T_ImageSource = ContextManager[Iterable[ImageArray]]
 def get_simple_image_source(
     path: str,
     image_size: ImageSize = None,
+    repeat: bool = None,
     **_
 ) -> T_ImageSource:
     local_image_path = get_file(path)
     LOGGER.debug('local_image_path: %r', local_image_path)
-    image_array = bgr_to_rgb(cv2.imread(local_image_path))
+    bgr_image_array = cv2.imread(local_image_path)
+    if bgr_image_array is None:
+        raise IOError('failed to load image: %r' % local_image_path)
+    image_array = bgr_to_rgb(bgr_image_array)
     if image_size is not None:
         image_array = resize_image_to(image_array, image_size)
-    yield [image_array]
+    image_array_iterable = [image_array]
+    if repeat:
+        image_array_iterable = cycle(image_array_iterable)
+    yield image_array_iterable
+
+
+def is_video_path(path: str) -> bool:
+    ext = os.path.splitext(os.path.basename(strip_url_suffix(path)))[-1]
+    LOGGER.debug('ext: %s', ext)
+    return ext.lower() in {'.webm', '.mkv', '.mp4'}
 
 
 def get_image_source_for_path(path: str, **kwargs) -> T_ImageSource:
+    if is_video_path(path):
+        return get_video_image_source(path, **kwargs)
     return get_simple_image_source(path, **kwargs)
--- layered_vision/utils/opencv.py
+++ layered_vision/utils/opencv.py
@@ -1,14 +1,98 @@
 import logging
-# from contextlib import contextmanager
-# from typing import ContextManager, Iterable
+from time import time, sleep
+from contextlib import contextmanager
+from typing import ContextManager, Iterable
 
 import cv2
 import numpy as np
 
-from .image import ImageArray, rgb_to_bgr
+from .image import ImageArray, ImageSize, rgb_to_bgr, bgr_to_rgb, get_image_size
 
 
 LOGGER = logging.getLogger(__name__)
+
+
+def iter_read_video_images(
+    video_capture: cv2.VideoCapture,
+    image_size: ImageSize = None,
+    interpolation: int = cv2.INTER_LINEAR,
+    fps: float = None,
+    repeat: bool = None
+) -> Iterable[np.ndarray]:
+    is_first = True
+    desired_frame_time = 1 / fps if fps else 0
+    last_frame_time = None
+    while True:
+        grabbed, image_array = video_capture.read()
+        if not grabbed:
+            LOGGER.info('video end reached')
+            if not repeat:
+                return
+            video_capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            grabbed, image_array = video_capture.read()
+            if not grabbed:
+                LOGGER.info('unable to rewind video')
+                return
+        LOGGER.debug('video image_array.shape: %s', image_array.shape)
+        if is_first:
+            LOGGER.info(
+                'received video image shape: %s (requested: %s)',
+                image_array.shape, image_size
+            )
+        if image_size and get_image_size(image_array) != image_size:
+            image_array = cv2.resize(
+                image_array,
+                (image_size.width, image_size.height),
+                interpolation=interpolation
+            )
+        rgb_image_array = bgr_to_rgb(image_array)
+        current_time = time()
+        if last_frame_time:
+            desired_wait_time = desired_frame_time - (current_time - last_frame_time)
+            if desired_wait_time > 0:
+                LOGGER.debug('sleeping for desired fps: %s', desired_wait_time)
+                sleep(desired_wait_time)
+        last_frame_time = time()
+        yield rgb_image_array
+        is_first = False
+
+
+@contextmanager
+def get_video_image_source(
+    path: str,
+    image_size: ImageSize = None,
+    repeat: bool = None,
+    fps: float = None,
+    **_
+) -> ContextManager[Iterable[np.ndarray]]:
+    LOGGER.info('loading video: %r', path)
+    video_capture = cv2.VideoCapture(path)
+    if image_size:
+        LOGGER.info('attempting to set vide image size to: %s', image_size)
+        video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, image_size.width)
+        video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, image_size.height)
+    if fps:
+        LOGGER.info('attempting to set vide fps to %r', fps)
+        video_capture.set(cv2.CAP_PROP_FPS, fps)
+    actual_image_size = ImageSize(
+        width=video_capture.get(cv2.CAP_PROP_FRAME_WIDTH),
+        height=video_capture.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    )
+    actual_fps = video_capture.get(cv2.CAP_PROP_FPS)
+    LOGGER.info(
+        'video reported image size: %s (%s fps)',
+        actual_image_size, actual_fps
+    )
+    try:
+        yield iter_read_video_images(
+            video_capture,
+            image_size=image_size,
+            fps=fps if fps is not None else actual_fps,
+            repeat=repeat
+        )
+    finally:
+        LOGGER.debug('releasing video capture: %s', path)
+        video_capture.release()
 
 
 class ShowImageSink:
