f2e696b3648958afaa281b614f7127fd7e44ece0
Daniel Ecer <de-code@users.noreply.github.com>
2020-10-22 23:15:29 +0100

    raise an exception if image couldn't be loaded

--- layered_vision/sources.py
+++ layered_vision/sources.py
@@ -22,7 +22,10 @@ def get_simple_image_source(
 ) -> T_ImageSource:
     local_image_path = get_file(path)
     LOGGER.debug('local_image_path: %r', local_image_path)
-    image_array = bgr_to_rgb(cv2.imread(local_image_path))
+    bgr_image_array = cv2.imread(local_image_path)
+    if bgr_image_array is None:
+        raise IOError('failed to load image: %r' % local_image_path)
+    image_array = bgr_to_rgb(bgr_image_array)
     if image_size is not None:
         image_array = resize_image_to(image_array, image_size)
     yield [image_array]
