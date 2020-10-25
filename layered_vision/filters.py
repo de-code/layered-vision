320c4000261e9bba9764bb0fa6fd46012314df79
Daniel Ecer <de-code@users.noreply.github.com>
2020-10-25 03:13:22 +0100

    added optional caching of bodypix resuts (#13)

--- layered_vision/filters.py
+++ layered_vision/filters.py
@@ -1,11 +1,12 @@
 import logging
 from abc import ABC, abstractmethod
+from time import time
 
 import numpy as np
 
 import tensorflow as tf
 from tf_bodypix.api import download_model, load_model, BodyPixModelPaths
-from tf_bodypix.model import BodyPixModelWrapper
+from tf_bodypix.model import BodyPixModelWrapper, BodyPixResultWrapper
 
 from .utils.image import (
     ImageArray,
@@ -70,6 +71,11 @@ class BodyPixFilter(AbstractLayerFilter):
         self._bodypix_model = None
         self.threshold = float(layer_config.get('threshold') or 0.50)
         self.internal_resolution = float(layer_config.get('internal_resolution') or 0.50)
+        self.cache_model_result_secs = float(
+            layer_config.get('cache_model_result_secs') or 0.0
+        )
+        self._bodypix_result_cache = None
+        self._bodypix_result_cache_time = None
 
     def load_bodypix_model(self) -> BodyPixModelWrapper:
         LOGGER.info('loading bodypix model: %s', self.model_path)
@@ -86,8 +92,19 @@ class BodyPixFilter(AbstractLayerFilter):
             self._bodypix_model = self.load_bodypix_model()
         return self._bodypix_model
 
+    def get_bodypix_result(self, image_array: ImageArray) -> BodyPixResultWrapper:
+        current_time = time()
+        if (
+            self._bodypix_result_cache is not None
+            and current_time < self._bodypix_result_cache_time + self.cache_model_result_secs
+        ):
+            return self._bodypix_result_cache
+        self._bodypix_result_cache = self.bodypix_model.predict_single(image_array)
+        self._bodypix_result_cache_time = current_time
+        return self._bodypix_result_cache
+
     def filter(self, image_array: ImageArray) -> ImageArray:
-        result = self.bodypix_model.predict_single(image_array)
+        result = self.get_bodypix_result(image_array)
         mask = result.get_mask(threshold=self.threshold, dtype=tf.float32) * 255
         LOGGER.debug('mask.shape: %s', mask.shape)
         return get_image_with_alpha(
