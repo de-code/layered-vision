900b384dd42557912fe6efaa2ce235172bf419da
Daniel Ecer <de-code@users.noreply.github.com>
2021-02-07 13:45:15 +0000

    testing peek and pop variant

--- tests/utils/opencv_test.py
+++ tests/utils/opencv_test.py
@@ -22,12 +22,20 @@ def get_pafy_stream(
 
 
 class TestReadLatestThreadedReader:
-    def test_should_return_last_read_item(self):
+    def test_should_return_last_read_item_using_peek(self):
         data_list = ['abc', 'def']
         with ReadLatestThreadedReader(iter(data_list)) as reader:
             # adding delay so that it reads to the last item
             sleep(0.01)
             peeked_data = reader.peek()
+        assert peeked_data == data_list[-1]
+
+    def test_should_return_last_read_item_using_pop(self):
+        data_list = ['abc', 'def']
+        with ReadLatestThreadedReader(iter(data_list)) as reader:
+            # adding delay so that it reads to the last item
+            sleep(0.01)
+            peeked_data = reader.pop()
         assert peeked_data == data_list[-1]
 
 
